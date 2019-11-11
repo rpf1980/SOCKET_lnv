aa33a3363e9433e87c22d915e60c0a92a187f063
rpf1980 <rafapolo1980@gmail.com>
2019-11-11 20:21:35 +0100

    chore(): modified main

--- ServidorChat/src/com/company/Main.java
+++ ServidorChat/src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        ServidorChat.lanzaServidor();
     }
 }
